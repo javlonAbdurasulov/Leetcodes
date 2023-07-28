9065db91c5372d8626cfbc105dd44663acd1291d
Javlon <abdurasulov2203@gmail.com>
2023-07-28 11:56:52 +0500

    Mutex potok

---
+++ Mutex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Leetcodes
+{
+    internal class Mutex
+    {
+        static Mutex mutexx = new Mutex();
+        static int x = 0;
+        public static void Run()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(start);
+                thread.Name = $"Thread: {i}";
+                thread.Start();
+            }
+
+            Console.ReadLine();
+
+
+        }
+        public static void start()
+        {
+            //mutexx.WaitOne();
+            x = 1;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}:{x}");
+                x++;
+                Thread.Sleep(100);
+            }
+            //mutexx.ReleaseMutex();
+
+        }
+    }
+
+}
---
+++ Potok.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcodes
+{
+    internal class Potok
+    {
+        static object _lock = new object();
+        static int[] ints = new int[Environment.ProcessorCount];
+        static int i = 0;
+        
+        static List<Task> tasks = new List<Task>();
+        public static async Task SumMassiv(int[] nums)
+        {
+
+            //Задача "Сумма элементов массива":
+            //Создайте массив случайных чисел и разделите его на несколько частей. Затем создайте несколько потоков, 
+            //каждый из которых будет вычислять сумму своей части массива, и затем суммируйте полученные результаты. Проверьте правильность результата.
+            int c = Environment.ProcessorCount;
+            int count = nums.Length / c;
+            int summ = 0;
+            for (int i = 0; i < c; i++)
+            {
+                tasks.Add(sum(nums, i * count, count));
+            }
+            Task.Delay(1000).Wait();
+            foreach (var task in ints)
+            {
+                summ+=task;
+            }
+            Console.WriteLine("sum:"+summ);
+
+
+        }
+        public static async Task sum(int[] nums,int a,int count)
+        {
+            int summ = 0;
+            await Task.Delay(100);
+            lock (_lock)
+            {
+                for (int i = a;i < nums.Length && i-a!=count;i++) { 
+                        summ+= nums[i];
+                    }
+                    ints[i] = summ;
+                    i++;
+            }
+        }
+    }
+}
